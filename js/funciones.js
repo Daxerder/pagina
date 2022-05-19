a7b4617b9b55d44ea5462d6f3e707dad1cf84bea
Daxerder <alrpintos@gmail.com>
2022-05-19 11:41:37 -0500

    primero

---
+++ js/funciones.js
@@ -0,0 +1,37 @@
+var mostrar = document.getElementsByClassName('mostrar_off');
+var ocultar = document.getElementsByClassName('ocultar');
+var prefix = document.getElementById('prefix');
+var span = document.getElementsByName('span');
+//var span2 = document.getElementsByName('span')
+console.log(span.length)
+//console.log(span2)
+
+
+function cambiar(){
+    for(var i = mostrar.length-1; i>-1; i--){
+        mostrar[i].classList.toggle('mostrar');        
+    }
+    for(var i = ocultar.length-1; i>-1; i--){
+        ocultar[i].classList.toggle('ocultar_off');
+    }
+}
+
+function guardar(){
+
+    alert("GUARDADO CON EXITACION EXITOSA!!!");
+    for(var i = 0; i < span.length; i++){
+        span[i].textContent = prefix.value;
+    }
+    /*aca ira pa cambiar el prefix :v*/
+    cambiar();
+}
+
+function reestablecer(){
+    alert("y volvemos a la normalidad");
+    prefix.value ="k<"
+    for(var i = 0; i < span.length; i++){
+        span[i].textContent = prefix.value;
+    }
+    
+    /*aca ira pa cambiar el prefix :v*/
+}
